14f039ac561e23b7f06ef300df2e09c5d0fd9f4b
Priyanka Bugade <priyankabugade86@gmail.com>
2025-01-28 15:42:57 -0500

    Add files via upload

---
+++ Files/remote_cache_poisoning/gen_dns_request.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+
+from scapy.all import *
+
+###############################
+# This is only a skeleton
+###############################
+
+
+with open('ip_req.bin', 'wb') as f:
+    f.write(bytes(pkt))
